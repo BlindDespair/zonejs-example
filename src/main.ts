8b9c62cf7932969fd304321b90758de0070c6f67
Ivan Soroka <ivan.soroka@supercharge.io>
2020-07-24 15:17:02 +0200

    chore: add onInvokeTask example

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,28 @@ declare const Zone: any;
 
 const myFirstZone = Zone.current.fork({
   name: 'my first zone',
+
+  onInvoke(parentZoneDelegate, _, targetZone, delegate, applyThis, applyArgs, source) {
+    console.log('we called run somewhere...');
+
+    return parentZoneDelegate.invoke(targetZone, delegate, applyThis, applyArgs, source);
+  },
+
+  onScheduleTask(parentZoneDelegate, _, targetZone, task) {
+    console.log('We called an async task somewhere and its callback will be called later in our zone...');
+
+    return parentZoneDelegate.scheduleTask(targetZone, task);
+  },
+
+  onInvokeTask(parentZoneDelegate, _, targetZone, task, applyThis, applyArgs) {
+    console.log('Async callback was called somewhere...');
+
+    return parentZoneDelegate.invokeTask(targetZone, task, applyThis, applyArgs);
+  },
 });
 
-console.log(myFirstZone.name); // my first
-
-console.log(myFirstZone.parent === Zone.current); // true
+myFirstZone.run(() => {
+  setTimeout(() => {
+    console.log('hi in 2 seconds');
+  }, 2000);
+});
