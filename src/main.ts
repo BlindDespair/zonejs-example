3f8e3f14d6fe550cdb01254301b037df3f5050c9
Ivan Soroka <ivan.soroka@supercharge.io>
2020-07-24 15:32:13 +0200

    chore: add onHasTask example

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,34 @@ declare const Zone: any;
 
 const myFirstZone = Zone.current.fork({
   name: 'my first zone',
+
+  onInvoke(parentZoneDelegate, _, targetZone, delegate, applyThis, applyArgs, source) {
+    console.log('we called run somewhere...');
+
+    return parentZoneDelegate.invoke(targetZone, delegate, applyThis, applyArgs, source);
+  },
+
+  onScheduleTask(parentZoneDelegate, _, targetZone, task) {
+    console.log('We called an async task somewhere and its callback will be called later in our zone...');
+
+    return parentZoneDelegate.scheduleTask(targetZone, task);
+  },
+
+  onInvokeTask(parentZoneDelegate, _, targetZone, task, applyThis, applyArgs) {
+    console.log('An async callback was called somewhere...');
+
+    return parentZoneDelegate.invokeTask(targetZone, task, applyThis, applyArgs);
+  },
+
+  onHasTask(parentZoneDelegate, _, targetZone, hasTaskState) {
+    console.log(hasTaskState);
+
+    return parentZoneDelegate.hasTask(targetZone, hasTaskState);
+  },
 });
 
-console.log(myFirstZone.name); // my first
-
-console.log(myFirstZone.parent === Zone.current); // true
+myFirstZone.run(() => {
+  setTimeout(() => {
+    console.log('hi in 2 seconds');
+  }, 2000);
+});
