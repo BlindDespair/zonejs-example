9724bf650576be46ac8d5ab99458cf7c6ab3ee0b
Ivan Soroka <ivan.soroka@supercharge.io>
2020-07-24 15:13:28 +0200

    chore: add example with use of onInvoke

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ declare const Zone: any;
 
 const myFirstZone = Zone.current.fork({
   name: 'my first zone',
+  onInvoke(parentZoneDelegate, _, targetZone, delegate, applyThis, applyArgs, source) {
+    console.log('we called `run` somewhere');
+
+    return parentZoneDelegate.invoke(targetZone, delegate, applyThis, applyArgs, source);
+  },
 });
 
-console.log(myFirstZone.name); // my first
-
-console.log(myFirstZone.parent === Zone.current); // true
+myFirstZone.run(() => {
+  console.log('hi from callback');
+});
